626cb58520891ee7188283f17d9e67d79192dde2
Timothy Fei Truong Jr <ttruong@ne47.bio>
2024-12-28 19:17:04 +0000

    even score mod is not necessary

--- examples/nested_fail.py
+++ examples/nested_fail.py
@@ -7,16 +7,21 @@ from torch.nn.attention.flex_attention import (
     create_block_mask,
     flex_attention,
     _mask_mod_signature,
-    _score_mod_signature,
     BlockMask,
 )
-
-from attn_gym.masks.document_mask import _offsets_to_doc_ids_tensor
 
 torch.set_default_dtype(torch.bfloat16)
 torch.set_default_device("cuda")
 create_block_mask = torch.compile(create_block_mask)
 flex_attention = torch.compile(flex_attention)
+
+
+def _offsets_to_doc_ids_tensor(offsets):
+    device = offsets.device
+    counts = offsets[1:] - offsets[:-1]
+    return torch.repeat_interleave(
+        torch.arange(len(counts), device=device, dtype=torch.int32), counts
+    )
 
 
 def generate_doc_mask_mod(offsets: Tensor) -> _mask_mod_signature:
@@ -26,16 +31,6 @@ def generate_doc_mask_mod(offsets: Tensor) -> _mask_mod_signature:
         return document_id[q_idx] == document_id[kv_idx]
 
     return doc_mask_mod
-
-
-def generate_doc_score_mod(offsets: Tensor) -> _score_mod_signature:
-    document_id = _offsets_to_doc_ids_tensor(offsets)
-
-    def doc_score_mod(score, b, h, q_idx, kv_idx):
-        same_doc = document_id[q_idx] == document_id[kv_idx]
-        return torch.where(same_doc, score, float("-inf"))
-
-    return doc_score_mod
 
 
 def block_mask_from_offsets(offsets: torch.Tensor) -> BlockMask:
@@ -54,15 +49,12 @@ def test_offsets(offsets: torch.Tensor, do_warmup: bool = False):
         _ = block_mask_from_offsets(offsets[:6])
     block_mask = block_mask_from_offsets(offsets)
 
-    # create score mod
-    score_mod = generate_doc_score_mod(offsets)
-
     # run forward and backward pass
     q = torch.rand(B, H, S, D, requires_grad=True)
     k = torch.rand(B, H, S, D, requires_grad=True)
     v = torch.rand(B, H, S, D, requires_grad=True)
     grad_out = torch.rand(B, H, S, D)
-    flex_out = flex_attention(q, k, v, score_mod=score_mod, block_mask=block_mask)
+    flex_out = flex_attention(q, k, v, score_mod=None, block_mask=block_mask)
     flex_out = cast(torch.Tensor, flex_out)
     flex_out.backward(grad_out)
 
