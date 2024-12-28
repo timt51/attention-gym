3fd957e6eb759dffb37eca1665677fe1022b920c
Timothy Fei Truong Jr <ttruong@ne47.bio>
2024-12-28 19:34:11 +0000

    just need to skip first block mask... issue only on nightly...

--- examples/nested_fail.py
+++ examples/nested_fail.py
@@ -2,13 +2,7 @@ import argparse
 from typing import cast
 
 import torch
-from torch import Tensor
-from torch.nn.attention.flex_attention import (
-    create_block_mask,
-    flex_attention,
-    _mask_mod_signature,
-    BlockMask,
-)
+from torch.nn.attention.flex_attention import create_block_mask, flex_attention
 
 torch.set_default_dtype(torch.bfloat16)
 torch.set_default_device("cuda")
@@ -16,58 +10,31 @@ create_block_mask = torch.compile(create_block_mask)
 flex_attention = torch.compile(flex_attention)
 
 
-def _offsets_to_doc_ids_tensor(offsets):
-    device = offsets.device
-    counts = offsets[1:] - offsets[:-1]
-    return torch.repeat_interleave(
-        torch.arange(len(counts), device=device, dtype=torch.int32), counts
-    )
-
-
-def generate_doc_mask_mod(offsets: Tensor) -> _mask_mod_signature:
-    document_id = _offsets_to_doc_ids_tensor(offsets)
-
-    def doc_mask_mod(b, h, q_idx, kv_idx):
-        return document_id[q_idx] == document_id[kv_idx]
-
-    return doc_mask_mod
-
-
-def block_mask_from_offsets(offsets: torch.Tensor) -> BlockMask:
-    S = int(offsets[-1].item())
-    mask_mod = generate_doc_mask_mod(offsets)
-    block_mask = create_block_mask(mask_mod=mask_mod, B=1, H=1, Q_LEN=S, KV_LEN=S)
-    return block_mask
-
-
-def test_offsets(offsets: torch.Tensor, do_warmup: bool = False):
-    # define problem size
-    B, H, S, D = 1, 1, int(offsets[-1].item()), 64
-
-    # create block mask
-    if do_warmup:
-        _ = block_mask_from_offsets(offsets[:6])
-    block_mask = block_mask_from_offsets(offsets)
-
-    # run forward and backward pass
-    q = torch.rand(B, H, S, D, requires_grad=True)
-    k = torch.rand(B, H, S, D, requires_grad=True)
-    v = torch.rand(B, H, S, D, requires_grad=True)
-    grad_out = torch.rand(B, H, S, D)
-    flex_out = flex_attention(q, k, v, score_mod=None, block_mask=block_mask)
-    flex_out = cast(torch.Tensor, flex_out)
-    flex_out.backward(grad_out)
+def mask_mod(b, h, q_idx, kv_idx):
+    return q_idx >= 0
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--do_warmup", action="store_true")
+    parser.add_argument("--skip_first_block_mask", action="store_true")
     args = parser.parse_args()
 
-    test_offsets(
-        offsets=torch.tensor([0, 125, 273, 365, 544, 712, 1161, 1311, 1478]),
-        do_warmup=args.do_warmup,
-    )
+    # define problem size
+    B, H, S, D = 1, 1, 1478, 64
+    for i, S in enumerate([712, 1478]):
+        # create block mask
+        block_mask = create_block_mask(mask_mod=mask_mod, B=1, H=1, Q_LEN=S, KV_LEN=S)
+        if i == 0 and args.skip_first_block_mask:
+            continue
+
+        # run forward and backward pass
+        q = torch.rand(B, H, S, D, requires_grad=True)
+        k = torch.rand(B, H, S, D, requires_grad=True)
+        v = torch.rand(B, H, S, D, requires_grad=True)
+        grad_out = torch.rand(B, H, S, D)
+        flex_out = flex_attention(q, k, v, score_mod=None, block_mask=block_mask)
+        flex_out = cast(torch.Tensor, flex_out)
+        flex_out.backward(grad_out)
 
 
 if __name__ == "__main__":
